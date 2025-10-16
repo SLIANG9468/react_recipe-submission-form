56585d63be8f572d9c1a4ef2e75dba948573a277
Sherri Liang-Zhou <sherriliang-zhou@MacBook-Pro-3.local>
2025-10-16 09:47:54 -0500

    draft RecipeSubmissionForm

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import RecipeSubmissionForm from "./components/RecipeSubmissionForm"
+function App() {
+
+  return (
+    <>
+      <RecipeSubmissionForm />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/RecipeSubmissionForm.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+
+
+const RecipeSubmissionForm = () => {
+    const [formData, setFormData] = useState({
+        title: '',
+        description: '',
+        servings: '',
+        difficulty_level:'',
+        category:'',
+        cuisine_type:''
+    })
+
+    
+    const handleChange = (event) =>{
+        const { name, value } = event.target //grabbing the name and value properties from the input element
+        setFormData(prevData => ({...prevData, [name]:value})) // creating new object, spreading old object, update the key that we are changing        
+    }
+
+  return (
+    <>
+        <form>
+            <input type="text" placeholder="title" value={formData.title} name="title" onChange={(event)=>handleChange(event)}/>
+            <input type="text" placeholder="description" value={formData.description} name="description" onChange={(event)=>handleChange(event)}/>
+            <input type="text" placeholder="servings" value={formData.servings} name="servings" onChange={(event)=>handleChange(event)}/>
+            <input type="text" placeholder="difficulty_level" value={formData.difficulty_level} name="difficulty_level" onChange={(event)=>handleChange(event)}/>
+            <input type="text" placeholder="category" value={formData.category} name="category" onChange={(event)=>handleChange(event)}/>
+            <input type="text" placeholder="cuisine_type" value={formData.cuisine_type} name="cuisine_type" onChange={(event)=>handleChange(event)}/>
+        </form>
+        <div>
+            <h3>Title: {formData.title}</h3>
+            <h3>Description: {formData.description}</h3>
+            <h3>Serving:{formData.servings}</h3>
+            <h3>Difficulty_Level: {formData.difficulty_level}</h3>
+            <h3>Category:{formData.category}</h3>
+            <h3>Cuision_Type:{formData.cuisine_type}</h3>
+        </div>
+    </>
+  )
+}
+
+export default RecipeSubmissionForm
